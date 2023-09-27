c0f909033c735ee9101c7a19ba62b5f9544ea582
fake-oskars <oskars@wrong.lv>
2023-09-27 15:47:01 +0300

    First test

---
+++ script.js
@@ -0,0 +1,41 @@
+// Check if the browser supports the Web Audio API
+if ('AudioContext' in window || 'webkitAudioContext' in window) {
+    const audioContext = new (window.AudioContext || window.webkitAudioContext)();
+    const analyser = audioContext.createAnalyser();
+    const micStream = navigator.mediaDevices.getUserMedia({ audio: true });
+
+    micStream.then((stream) => {
+        const source = audioContext.createMediaStreamSource(stream);
+        source.connect(analyser);
+        analyser.connect(audioContext.destination);
+
+        analyser.fftSize = 256; // Adjust this value for sensitivity
+
+        const dataArray = new Uint8Array(analyser.frequencyBinCount);
+
+        const container = document.querySelector('.container');
+
+        function updateColor() {
+            analyser.getByteFrequencyData(dataArray);
+
+            // Calculate the average volume
+            const averageVolume = dataArray.reduce((sum, value) => sum + value, 0) / dataArray.length;
+
+            // Map the volume to a color (adjust the values as needed)
+            const red = Math.floor(255 * (averageVolume / 255));
+            const green = Math.floor(255 * (1 - averageVolume / 255));
+            const blue = 100;
+
+            // Set the background color based on volume
+            container.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
+
+            requestAnimationFrame(updateColor);
+        }
+
+        updateColor();
+    }).catch((error) => {
+        console.error('Error accessing the microphone:', error);
+    });
+} else {
+    console.error('Web Audio API is not supported in this browser.');
+}
