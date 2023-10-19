98c3039e08ef71a9f173437ab938c1aaf4cabcc0
fake-oskars <51405688+fake-oskars@users.noreply.github.com>
2023-10-19 11:15:09 +0300

    Update script.js

--- script.js
+++ script.js
@@ -2,84 +2,74 @@
 if ('AudioContext' in window || 'webkitAudioContext' in window) {
     const audioContext = new (window.AudioContext || window.webkitAudioContext)();
     const analyser = audioContext.createAnalyser();
-    let micStream;
+    const micStream = navigator.mediaDevices.getUserMedia({ audio: true });
 
-    const circle = document.getElementById('circle');
+    micStream.then((stream) => {
+        const source = audioContext.createMediaStreamSource(stream);
+        source.connect(analyser);
 
-    function startMicrophone() {
-        navigator.mediaDevices.getUserMedia({ audio: true })
-            .then((stream) => {
-                const source = audioContext.createMediaStreamSource(stream);
-                source.connect(analyser);
+        // Initial value for sensitivity
+        let sensitivity = 32;
 
-                analyser.fftSize = 32; // Adjust this value for sensitivity
+        // Get the slider element
+        const sensitivitySlider = document.getElementById('sensitivity');
 
-                const dataArray = new Uint8Array(analyser.frequencyBinCount);
+        // Get the circle element
+        const circle = document.getElementById('circle');
 
-                function updateColorAndSize() {
-                    analyser.getByteFrequencyData(dataArray);
+        // Event listener for slider input
+        sensitivitySlider.addEventListener('input', function () {
+            sensitivity = parseInt(this.value);
+            analyser.fftSize = sensitivity;
+        });
 
-                    // Calculate the average volume
-                    const averageVolume = dataArray.reduce((sum, value) => sum + value, 0) / dataArray.length;
+        analyser.fftSize = sensitivity;
 
-                    // Map the volume to a color gradient from blue to red
-                    const blue = Math.floor(255 * (1 - averageVolume / 255));
-                    const red = Math.floor(255 * (averageVolume / 255));
+        const dataArray = new Uint8Array(analyser.frequencyBinCount);
 
-                    // Set the background color based on volume
-                    document.body.style.backgroundColor = `rgb(${red}, 0, ${blue})`;
+        function updateColorAndSize() {
+            analyser.getByteFrequencyData(dataArray);
 
-                    // Set the size of the circle based on volume
-                    const circleSize = averageVolume;
-                    circle.style.width = `${circleSize}px`;
-                    circle.style.height = `${circleSize}px`;
+            // Calculate the average volume
+            const averageVolume = dataArray.reduce((sum, value) => sum + value, 0) / dataArray.length;
 
-                    requestAnimationFrame(updateColorAndSize);
-                }
+            // Map the volume to a color gradient from blue to red
+            const blue = Math.floor(255 * (1 - averageVolume / 255));
+            const red = Math.floor(255 * (averageVolume / 255));
 
-                updateColorAndSize();
-            })
-            .catch((error) => {
-                console.error('Error accessing the microphone:', error);
-            });
-    }
+            // Set the background color based on volume
+            document.body.style.backgroundColor = `rgb(${red}, 0, ${blue})`;
 
-    function stopMicrophone() {
-        if (micStream) {
-            const tracks = micStream.getTracks();
-            tracks.forEach(track => track.stop());
+            // Set the size of the circle based on sensitivity and volume
+            const circleSize = sensitivity + averageVolume;
+            circle.style.width = `${circleSize}px`;
+            circle.style.height = `${circleSize}px`;
+
+            requestAnimationFrame(updateColorAndSize);
         }
-    }
 
-    // Start microphone when page becomes visible
-    document.addEventListener('visibilitychange', function () {
-        if (document.visibilityState === 'visible') {
-            startMicrophone();
-        } else {
-            stopMicrophone();
-        }
+        updateColorAndSize();
+    }).catch((error) => {
+        console.error('Error accessing the microphone:', error);
     });
-
-    // Function to update the clock with the correct timezone
-    function updateClock() {
-        const options = {
-            timeZone: 'Europe/Riga', // Timezone for Latvia (GMT+2)
-            hour12: false, // Use 24-hour format
-            hour: '2-digit',
-            minute: '2-digit',
-            second: '2-digit',
-            fractionalSecondDigits: 3, // Display milliseconds
-        };
-
-        const currentTime = new Date().toLocaleTimeString('en-US', options);
-        document.getElementById('clock').textContent = currentTime;
-    }
-
-    // Update the clock every millisecond
-    setInterval(updateClock, 1);
-
-    // Start the microphone initially
-    startMicrophone();
 } else {
     console.error('Web Audio API is not supported in this browser.');
 }
+
+// Function to update the clock with the correct timezone
+function updateClock() {
+    const options = {
+        timeZone: 'Europe/Riga', // Timezone for Latvia (GMT+2)
+        hour12: false, // Use 24-hour format
+        hour: '2-digit',
+        minute: '2-digit',
+        second: '2-digit',
+        fractionalSecondDigits: 3, // Display milliseconds
+    };
+
+    const currentTime = new Date().toLocaleTimeString('en-US', options);
+    document.getElementById('clock').textContent = currentTime;
+}
+
+// Update the clock every millisecond
+setInterval(updateClock, 1);
