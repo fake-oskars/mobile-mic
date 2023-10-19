d07fea1a15e7c6e936140676ec9b753889b6e4d0
fake-oskars <51405688+fake-oskars@users.noreply.github.com>
2023-10-19 10:48:34 +0300

    debug 3

--- script.js
+++ script.js
@@ -17,16 +17,9 @@ if ('AudioContext' in window || 'webkitAudioContext' in window) {
             // Calculate the average volume
             const averageVolume = Math.abs(dataArray.reduce((sum, value) => sum + value, 0) / dataArray.length);
 
-            // Map the volume to a more complex color gradient
-            const hue = 240 - (averageVolume * 120); // Blue to red
-            const saturation = 100;
-            const lightness = 50;
-
-            // Convert HSL to RGB
-            const rgb = hslToRgb(hue / 360, saturation / 100, lightness / 100);
-
-            // Set the background color based on volume
-            document.body.style.backgroundColor = `rgb(${rgb[0]}, ${rgb[1]}, ${rgb[2]})`;
+            // Map the volume to a color gradient from black to red
+            const intensity = Math.floor(255 * averageVolume);
+            document.body.style.backgroundColor = `rgb(${intensity}, 0, 0)`;
 
             requestAnimationFrame(updateColor);
         }
@@ -37,32 +30,6 @@ if ('AudioContext' in window || 'webkitAudioContext' in window) {
     });
 } else {
     console.error('Web Audio API is not supported in this browser.');
-}
-
-// Function to convert HSL to RGB
-function hslToRgb(h, s, l) {
-    let r, g, b;
-
-    if (s === 0) {
-        r = g = b = l; // achromatic
-    } else {
-        const hue2rgb = (p, q, t) => {
-            if (t < 0) t += 1;
-            if (t > 1) t -= 1;
-            if (t < 1 / 6) return p + (q - p) * 6 * t;
-            if (t < 1 / 2) return q;
-            if (t < 2 / 3) return p + (q - p) * (2 / 3 - t) * 6;
-            return p;
-        };
-
-        const q = l < 0.5 ? l * (1 + s) : l + s - l * s;
-        const p = 2 * l - q;
-        r = hue2rgb(p, q, h + 1 / 3);
-        g = hue2rgb(p, q, h);
-        b = hue2rgb(p, q, h - 1 / 3);
-    }
-
-    return [Math.round(r * 255), Math.round(g * 255), Math.round(b * 255)];
 }
 
 
